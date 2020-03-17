4339273da8ab52586d2dc46930ef88927bbd47c6
Aniko <bartos.aniko.job@gmail.com>
2020-03-17 21:37:27 +0100

    carramba! corrected test

--- test/tst_example.cpp
+++ test/tst_example.cpp
@@ -21,13 +21,13 @@ TEST(AddTest, Add_2_5)
 
 TEST(MultTest, Mult_1_3)
 {
-    int a = example::add(1, 3);
+    int a = example::mult(1, 3);
     EXPECT_EQ(3, a);
 }
 
 TEST(MultTest, Add_2_5)
 {
-    int a = example::add(2, 5);
+    int a = example::mult(2, 5);
     EXPECT_EQ(10, a);
 }
 
@@ -45,14 +45,14 @@ TEST(NegTest, Neg_minus3)
 
 TEST(divTest, Div_3_1)
 {
-    double a = example::add(3, 1);
+    double a = example::div(3, 1);
     EXPECT_EQ(3, a);
 }
 
 TEST(divTest, Div_5_10)
 {
-    double a = example::add(5, 10);
-    EXPECT_EQ(0.2, a);
+    double a = example::div(5, 10);
+    EXPECT_EQ(0.5, a);
 }
 
 
